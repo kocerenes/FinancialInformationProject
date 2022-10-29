816eb333d4f2d562fcaa71c950f9f7d81e599d7b
herdal06 <h.erdal721@gmail.com>
2022-10-29 10:58:52 +0300

    Add dependencies

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,23 @@
+object Versions {
+
+    const val coreKtx = "1.7.0"
+    const val appCompat = "1.5.1"
+    const val material = "1.7.0"
+    const val constraint = "2.1.4"
+    const val viewModel = "2.5.1"
+    const val livedata = "2.5.1"
+    const val navigation = "2.5.2"
+    const val junit = "4.+"
+    const val testExt = "1.1.3"
+    const val espresso = "3.4.0"
+    const val room = "2.4.3"
+    const val hilt = "2.42"
+    const val hiltCompiler = "2.42"
+    const val safeArgs = "2.5.2"
+    const val gson = "2.9.0"
+    const val runtime = "2.5.1"
+    const val extensions = "2.2.0"
+    const val retrofit = "2.9.0"
+    const val ok_http = "4.7.2"
+    const val glide = "4.14.2"
+}
