33fdaa7a663a01330efe3dbc5124c6724768ddbc
kocerenes <eneskocerr41@gmail.com>
2022-11-03 17:16:06 +0300

    Create the Dependency Injection with Hilt

---
+++ app/src/main/java/com/ekheek/financialinformationproject/di/FinancialApp.kt
@@ -0,0 +1,8 @@
+package com.ekheek.financialinformationproject.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FinancialApp : Application() {
+}
--- app/src/main/java/com/ekheek/financialinformationproject/presentation/MainActivity.kt
+++ app/src/main/java/com/ekheek/financialinformationproject/presentation/MainActivity.kt
@@ -3,7 +3,9 @@ package com.ekheek.financialinformationproject.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ekheek.financialinformationproject.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
