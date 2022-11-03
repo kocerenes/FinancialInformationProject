9edfbc8c1b76e2f31bb480b351046d9866518b0a
kocerenes <eneskocerr41@gmail.com>
2022-11-03 14:29:14 +0300

    Change file location MainActivity

--- app/src/main/java/com/ekheek/financialinformationproject/presentation/MainActivity.kt
+++ app/src/main/java/com/ekheek/financialinformationproject/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ekheek.financialinformationproject
+package com.ekheek.financialinformationproject.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ekheek.financialinformationproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
