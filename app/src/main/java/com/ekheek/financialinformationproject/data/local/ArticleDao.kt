9cf187833a4b855812919475146c005d4d6f9e19
emirk <emir.karabey1@hotmail.com>
2022-12-02 20:33:51 +0300

    Change delete favorite function in article dao

--- app/src/main/java/com/ekheek/financialinformationproject/data/local/ArticleDao.kt
+++ app/src/main/java/com/ekheek/financialinformationproject/data/local/ArticleDao.kt
@@ -1,11 +1,11 @@
 package com.ekheek.financialinformationproject.data.local
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.ekheek.financialinformationproject.data.local.entity.FavoriteEntity
+import com.ekheek.financialinformationproject.data.remote.model.Article
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -15,11 +15,11 @@ interface ArticleDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertFavoriteArticle(favoritesEntity: FavoriteEntity)
 
-    @Query("SELECT * FROM favorites_article_table ORDER BY id ASC")
+    @Query("SELECT * FROM favorites_article_table ORDER BY id DESC")
     fun readFavoriteArticles(): Flow<List<FavoriteEntity>>
 
-    @Delete
-    suspend fun deleteFavoriteArticle(favoritesEntity: FavoriteEntity)
+    @Query("DELETE FROM favorites_article_table WHERE article=:article")
+    suspend fun deleteFavoriteArticle(article: Article)
 
     @Query("DELETE FROM favorites_article_table")
     suspend fun deleteAllFavoriteArticles()
