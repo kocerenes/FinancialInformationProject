3ce3d49dd9c0fed90c2a04f725f0922e682804f4
emirk <emir.karabey1@hotmail.com>
2022-12-02 20:56:05 +0300

    Conflict fixed

--- app/src/main/java/com/ekheek/financialinformationproject/presentation/MainActivity.kt
+++ app/src/main/java/com/ekheek/financialinformationproject/presentation/MainActivity.kt
@@ -18,7 +18,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         supportActionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
