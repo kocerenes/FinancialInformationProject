b5c7eb1fbc0824c668cc5cb2bbb1f00ff7bc5100
herdal06 <h.erdal721@gmail.com>
2022-12-16 00:08:49 +0300

    Change country code tr to us

--- app/src/main/java/com/ekheek/financialinformationproject/util/Constants.kt
+++ app/src/main/java/com/ekheek/financialinformationproject/util/Constants.kt
@@ -7,7 +7,7 @@ object Constants {
     const val NEWS_BASE_URL = "https://newsapi.org/"
     const val NEWS_END_POINT = "v2/top-headlines"
     const val NEWS_API_KEY = BuildConfig.NEWS_API_KEY
-    const val NEWS_COUNTRY_CODE = "tr"
+    const val NEWS_COUNTRY_CODE = "us"
     const val SEARCH_NEWS_END_POINT = "v2/everything"
 
     //Favorites Article
