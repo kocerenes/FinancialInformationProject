d9e695537b6fe0f06fc4ecbd178280a794cdb053
herdal06 <h.erdal721@gmail.com>
2022-12-16 11:25:09 +0300

    Show toast message on star icon click

--- app/src/main/java/com/ekheek/financialinformationproject/presentation/detail/NewsDetailFragment.kt
+++ app/src/main/java/com/ekheek/financialinformationproject/presentation/detail/NewsDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -87,8 +88,10 @@ class NewsDetailFragment : BottomSheetDialogFragment() {
         if (detailViewModel.favLiveData.value == true) {
             detailViewModel.favLiveData.value = false
             detailViewModel.deleteFavoriteNews(getArticle())
+            Toast.makeText(requireContext(),getString(R.string.removed_from_favorite),Toast.LENGTH_SHORT).show()
         } else {
             detailViewModel.favLiveData.value = true
+            Toast.makeText(requireContext(),getString(R.string.added_to_favorite),Toast.LENGTH_SHORT).show()
             detailViewModel.addFavoriteNews(FavoriteEntity(article = getArticle(), id = 0))
         }
     }
