9f9cc36a151287fe924b7bf3a5250816f4e0fa6b
herdal06 <h.erdal721@gmail.com>
2022-12-16 11:35:10 +0300

    Change detail dialog to fragment, add animations while navigating home to detail

--- app/src/main/java/com/ekheek/financialinformationproject/presentation/detail/NewsDetailFragment.kt
+++ app/src/main/java/com/ekheek/financialinformationproject/presentation/detail/NewsDetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -13,11 +14,10 @@ import com.ekheek.financialinformationproject.R
 import com.ekheek.financialinformationproject.data.local.entity.FavoriteEntity
 import com.ekheek.financialinformationproject.data.remote.model.Article
 import com.ekheek.financialinformationproject.databinding.FragmentNewsDetailBinding
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class NewsDetailFragment : BottomSheetDialogFragment() {
+class NewsDetailFragment : Fragment() {
 
     private var _binding: FragmentNewsDetailBinding? = null
 
