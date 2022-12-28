31ebd7d9ff3fe17dcd8b1ac430754b79b24afd7a
kocerenes <eneskocerr41@gmail.com>
2022-12-28 23:24:31 +0300

    Code refactor

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -2,7 +2,7 @@ object Config {
     const val compileSdk = 32
     const val minSdk = 23
     const val targetSdk = 32
-    const val versionCode = 2
-    const val versionName = "1.1"
+    const val versionCode = 3
+    const val versionName = "1.2"
     const val applicationId = "com.ekheek.financialinformationproject"
 }
